700a373e59227ac5d53f757a9f6dbb338d366d62
Maxim Khatskevich <maxim@khatskevi.ch>
2018-10-25 21:54:12 +0500

    Setup basic project structure

---
+++ Sources/App.swift
@@ -0,0 +1,19 @@
+//
+//  App.swift
+//  WeatherNow
+//
+//  Created by Maxim Khatskevich on 10/25/18.
+//  Copyright © 2018 Maxim Khatskevich. All rights reserved.
+//
+
+import UIKit
+
+//---
+
+@UIApplicationMain
+class App: UIResponder
+{
+    // MARK: UIApplicationDelegate support
+    
+    var window: UIWindow? = UIWindow()
+}
---
+++ Sources/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  WeatherNow
+//
+//  Created by Maxim Khatskevich on 10/25/18.
+//  Copyright © 2018 Maxim Khatskevich. All rights reserved.
+//
+
+import UIKit
+
+//---
+
+extension App: UIApplicationDelegate
+{
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
