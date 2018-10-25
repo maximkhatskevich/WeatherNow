5ad54c968cd8fd875c1db6efb69127dd3af362a3
Maxim Khatskevich <maxim@khatskevi.ch>
2018-10-25 21:37:26 +0500

    Initial Commit

---
+++ WeatherNow/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherNow
+//
+//  Created by Maxim Khatskevich on 10/25/18.
+//  Copyright © 2018 Maxim Khatskevich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
